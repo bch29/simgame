f46d18bdbcd864d1bfafc9303dc929b0be5cdbb9
Bradley Hardy <bradleyhardy@live.com>
2019-11-30 19:37:15 -0500

    Smooth camera controls

--- render/src/test.rs
+++ render/src/test.rs
@@ -5,7 +5,7 @@ use winit::{
     event_loop::{ControlFlow, EventLoop},
 };
 use simgame_core::world::{World, UpdatedWorldState};
-use cgmath::{Vector3, InnerSpace};
+use cgmath::{Vector3, InnerSpace, Zero};
 
 pub fn build_window(
     event_loop: &winit::event_loop::EventLoop<()>,
@@ -15,6 +15,33 @@ pub fn build_window(
     // .with_fullscreen(Some(winit::window::Fullscreen::Borderless(monitor)))
     // .with_decorations(true);
     Ok(builder.build(event_loop)?)
+}
+
+struct ControlState {
+    forward: i32,
+    right: i32,
+    up: i32
+}
+
+impl ControlState {
+    fn camera_delta(&self) -> Vector3<f32> {
+        let speed = 0.4;
+        let forward_step = Vector3::new(1., 1., 0.).normalize();
+        let right_step = Vector3::new(1., -1., 0.).normalize();
+        let up_step = Vector3::new(0., 0., 1.).normalize();
+
+        let mut dir = Vector3::zero();
+
+        dir += forward_step * self.forward as f32;
+        dir += right_step * self.right as f32;
+        dir += up_step * self.up as f32;
+
+        if dir.magnitude() > 0.01 {
+            dir.normalize() * speed
+        } else {
+            Vector3::zero()
+        }
+    }
 }
 
 pub fn test_render(world: World, vert_shader: &[u8], frag_shader: &[u8]) -> Result<()> {
@@ -37,14 +64,23 @@ pub fn test_render(world: World, vert_shader: &[u8], frag_shader: &[u8]) -> Resu
         },
     };
 
-    let forward_step = Vector3::new(1., 1., 0.).normalize();
-    let right_step = Vector3::new(1., -1., 0.).normalize();
-    let up_step = Vector3::new(0., 0., 1.).normalize();
-
     let mut render_state = RenderState::new(render_init)?;
     render_state.init(&world);
 
     use event::{Event, WindowEvent, VirtualKeyCode};
+
+    let mut control_state = ControlState {
+        forward: 0,
+        right: 0,
+        up: 0,
+    };
+
+    fn state_to_mag(state: event::ElementState) -> i32 {
+        match state {
+            event::ElementState::Pressed => 1,
+            event::ElementState::Released => 0,
+        }
+    }
 
     event_loop.run(move |event, _, control_flow| {
         *control_flow = if cfg!(feature = "metal-auto-capture") {
@@ -70,17 +106,17 @@ pub fn test_render(world: World, vert_shader: &[u8], frag_shader: &[u8]) -> Resu
                     input:
                         event::KeyboardInput {
                             virtual_keycode: Some(key),
-                            state: event::ElementState::Pressed,
+                            state,
                             ..
                         },
                     ..
                 } => match key {
-                    VirtualKeyCode::W => render_state.update_camera_pos(forward_step),
-                    VirtualKeyCode::S => render_state.update_camera_pos(-forward_step),
-                    VirtualKeyCode::D => render_state.update_camera_pos(right_step),
-                    VirtualKeyCode::A => render_state.update_camera_pos(-right_step),
-                    VirtualKeyCode::J => render_state.update_camera_pos(-up_step),
-                    VirtualKeyCode::K => render_state.update_camera_pos(up_step),
+                    VirtualKeyCode::W => control_state.forward = state_to_mag(state),
+                    VirtualKeyCode::S => control_state.forward = -state_to_mag(state),
+                    VirtualKeyCode::D => control_state.right = state_to_mag(state),
+                    VirtualKeyCode::A => control_state.right = -state_to_mag(state),
+                    VirtualKeyCode::K => control_state.up = state_to_mag(state),
+                    VirtualKeyCode::J => control_state.up = -state_to_mag(state),
                     _ => {}
                 }
                 _ => {}
@@ -90,6 +126,8 @@ pub fn test_render(world: World, vert_shader: &[u8], frag_shader: &[u8]) -> Resu
                 render_state.render_frame();
             },
             _ => (),
-        }
+        };
+
+        render_state.update_camera_pos(control_state.camera_delta());
     });
 }
