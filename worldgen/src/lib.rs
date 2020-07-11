882fc974651976e0c7811a71700018e6ee11a17a
Bradley Hardy <bradleyhardy29@gmail.com>
2020-07-11 10:59:30 +0100

    clean up worldgen code

--- worldgen/src/lib.rs
+++ worldgen/src/lib.rs
@@ -1,17 +1,17 @@
 use anyhow::Result;
-use cgmath::Vector3;
+use cgmath::{EuclideanSpace, Point3, Vector3};
 use log::info;
 use rand::Rng;
 use serde::{Deserialize, Serialize};
 
-use simgame_core::block::{Block, WorldBlockData, BlockConfigHelper};
-use simgame_core::world::{BlockUpdater, UpdatedWorldState};
+use simgame_core::block::{Block, BlockConfigHelper, WorldBlockData};
 use simgame_core::util::Bounds;
+use simgame_core::world::{BlockUpdater, UpdatedWorldState};
 
-pub mod primitives;
 pub mod lsystem;
-pub mod turtle;
+pub mod primitives;
 pub mod tree;
+pub mod turtle;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct GenerateWorldConfig {
@@ -19,77 +19,140 @@ pub struct GenerateWorldConfig {
     pub tree: Option<tree::TreeConfig>,
 }
 
-pub fn generate_world(config: &GenerateWorldConfig, block_helper: &BlockConfigHelper) -> Result<WorldBlockData> {
+pub struct WorldGenerator<'a, R> {
+    config: &'a GenerateWorldConfig,
+    block_helper: &'a BlockConfigHelper,
+    bounds: Bounds<i64>,
+    blocks: WorldBlockData,
+    rng: &'a mut R,
+}
+
+pub fn generate_world(
+    config: &GenerateWorldConfig,
+    block_helper: &BlockConfigHelper,
+) -> Result<WorldBlockData> {
     info!("Creating empty world: {:?}", config);
-    let world_bounds = Bounds::from_size(config.size);
-    let mut blocks = WorldBlockData::empty(world_bounds);
 
-    let world_size = Vector3 {
-        x: config.size.x as f64,
-        y: config.size.y as f64,
-        z: config.size.z as f64,
-    };
-
-    info!("Generating terrain");
-    let base_z = 0.0;
-    let terrain_height = world_size.z;
-
-    let cos_factor = 2.0;
-    let sin_factor = 1.0;
-
-    let mut blocks_done = 0;
-    let total_blocks = config.size.x * config.size.y * config.size.z;
-
-    let progress_count = 10;
-    let mut next_progress_step = 1;
-
-    info!("Bounds are {:?}", world_bounds);
     let mut rng = rand::thread_rng();
-    blocks
-        .replace_blocks(world_bounds, |loc, _| -> Result<Block> {
-            // Normalized point with coords in range (0, 1)
-            let p = Vector3 {
-                x: loc.x as f64 / world_size.x,
-                y: loc.y as f64 / world_size.y,
-                z: loc.z as f64 / world_size.z,
-            };
 
-            // Empty above curve, filled below curve
-            use std::f64::consts::PI;
-            // val in range [0, 1]
-            let val = 0.5 * (1.0 + (p.x * PI * sin_factor).sin() * (p.y * PI * cos_factor).cos());
-            assert!(0. <= val && val <= 1.);
+    let mut generator = WorldGenerator::new(config, block_helper, &mut rng);
+    generator.generate()?;
+    Ok(generator.finish())
+}
 
-            // this is the height of the terrain at current x, y coordinate
-            let height_here = base_z + val * terrain_height;
+impl<'a, R> WorldGenerator<'a, R> {
+    pub fn new(
+        config: &'a GenerateWorldConfig,
+        block_helper: &'a BlockConfigHelper,
+        rng: &'a mut R,
+    ) -> Self {
+        let bounds = {
+            let mut origin = Point3::origin() - config.size / 2;
+            origin.z = 0;
+            Bounds::new(origin, config.size)
+        };
 
-            // fill with air if current z is above height_here, else rock
-            let block = if p.z * world_size.z > height_here {
-                Block::air()
-            } else {
-                let block_val = 1 + rng.gen::<u16>() % 8;
-                Block::from_u16(block_val)
-            };
+        let blocks = WorldBlockData::empty(bounds);
 
-            blocks_done += 1;
-            if blocks_done == (next_progress_step * total_blocks) / progress_count {
-                info!("{}%", next_progress_step * 10);
-                next_progress_step += 1;
-            }
-
-            Ok(block)
-        })
-        .unwrap();
-
-    if let Some(tree_config) = &config.tree {
-        let mut updated_state = UpdatedWorldState::empty();
-        let mut block_updater = BlockUpdater::new(&mut blocks, &mut updated_state);
-
-        let tree = tree::generate(tree_config, block_helper, &mut rng)?;
-        tree.draw(&mut block_updater);
+        Self {
+            config,
+            block_helper,
+            bounds,
+            blocks,
+            rng,
+        }
     }
 
-    info!("Generated world: {:?}", blocks.debug_summary());
+    pub fn generate(&mut self) -> Result<()>
+    where
+        R: Rng,
+    {
+        self.generate_terrain()?;
+        self.generate_trees()?;
+        Ok(())
+    }
 
-    Ok(blocks)
+    pub fn finish(self) -> WorldBlockData {
+        self.blocks
+    }
+
+    fn generate_terrain(&mut self) -> Result<()>
+    where
+        R: Rng,
+    {
+        info!("Generating terrain");
+        let world_size = Vector3 {
+            x: self.config.size.x as f64,
+            y: self.config.size.y as f64,
+            z: self.config.size.z as f64,
+        } / 2.;
+
+        let base_z = 0.0;
+        let terrain_height = world_size.z;
+
+        let cos_factor = 2.0;
+        let sin_factor = 1.0;
+
+        let mut blocks_done = 0;
+        let total_blocks = self.config.size.x * self.config.size.y * self.config.size.z;
+
+        let progress_count = 10;
+        let mut next_progress_step = 1;
+
+        info!("Bounds are {:?}", self.bounds);
+        let mut rng = rand::thread_rng();
+        self.blocks
+            .replace_blocks(self.bounds, |loc, _| -> Result<Block> {
+                // Normalized point with coords in range (0, 1)
+                let p = Vector3 {
+                    x: loc.x as f64 / world_size.x,
+                    y: loc.y as f64 / world_size.y,
+                    z: loc.z as f64 / world_size.z,
+                };
+
+                // Empty above curve, filled below curve
+                use std::f64::consts::PI;
+                // val in range [0, 1]
+                let val =
+                    0.5 * (1.0 + (p.x * PI * sin_factor).sin() * (p.y * PI * cos_factor).cos());
+                assert!(0. <= val && val <= 1.);
+
+                // this is the height of the terrain at current x, y coordinate
+                let height_here = base_z + val * terrain_height;
+
+                // fill with air if current z is above height_here, else rock
+                let block = if p.z * world_size.z > height_here {
+                    Block::air()
+                } else {
+                    let block_val = 1 + rng.gen::<u16>() % 8;
+                    Block::from_u16(block_val)
+                };
+
+                blocks_done += 1;
+                if blocks_done == (next_progress_step * total_blocks) / progress_count {
+                    info!("{}%", next_progress_step * 10);
+                    next_progress_step += 1;
+                }
+
+                Ok(block)
+            })
+    }
+
+    fn generate_trees(&mut self) -> Result<()>
+    where
+        R: Rng,
+    {
+        let tree_config = match &self.config.tree {
+            Some(x) => x,
+            None => return Ok(()),
+        };
+
+        let mut updated_state = UpdatedWorldState::empty();
+        let mut block_updater = BlockUpdater::new(&mut self.blocks, &mut updated_state);
+
+        let tree = tree::generate(tree_config, self.block_helper, &mut self.rng)?;
+        tree.draw(&mut block_updater);
+
+        Ok(())
+    }
 }
