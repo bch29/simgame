b60b55ccc3af83655b321ad22a36d7218c15e34a
Bradley Hardy <bradleyhardy29@gmail.com>
2021-03-08 22:56:16 +0000

    triangulating spheres

---
+++ render/src/mesh/sphere.rs
@@ -0,0 +1,66 @@
+use crate::mesh::{Mesh, Vertex};
+
+use zerocopy::{AsBytes, FromBytes};
+
+/// A "face" of a unit sphere which has been triangulated by projecting a subdivided cube onto the
+/// sphere.
+#[derive(Clone, Copy, Debug, AsBytes, FromBytes, Default)]
+#[repr(C)]
+pub struct UnitSphereFace {
+    pub detail: u32,
+}
+
+impl UnitSphereFace {
+    /// Triangle the sphere
+    pub fn mesh(&self) -> Mesh {
+        let mut vertices = Vec::new();
+        let mut indices = Vec::new();
+
+        let half_pi = std::f32::consts::PI / 2.;
+        let pi = std::f32::consts::PI;
+
+        let n = 1 + self.detail;
+
+        for i in 0..n {
+            for j in 0..n {
+                // (square_x, square_y) are coordinates within the square face that we are
+                // projecting from
+                let square_x = i as f32 / self.detail as f32;
+                let square_y = j as f32 / self.detail as f32;
+
+                // (theta, phi) are the angular coordinates within the sphere
+                let theta = -half_pi + pi * square_x;
+                let phi = -half_pi + pi * square_y;
+
+                // (x, y, z) are the coordinates of the point on the sphere in 3D Euclidean space
+                let x = theta.cos();
+                let y = theta.sin();
+                let z = phi.sin();
+
+                vertices.push(Vertex {
+                    pos: [x, y, z, 1.],
+                    normal: [x, y, z],
+                    tex_coord: [square_x, square_y],
+                });
+
+                // each vertex adds indices for the two triangles forming the square below and to
+                // its right
+                if i < n - 1 && j < n - 1 {
+                    indices.push(j + i * n);
+                    indices.push((1 + j) + i * n);
+                    indices.push((1 + j) + (1 + i) * n);
+
+                    indices.push((1 + j) + (1 + i) * n);
+                    indices.push(j + (1 + i) * n);
+                    indices.push(j + i * n);
+                }
+            }
+        }
+
+        Mesh { vertices, indices }
+    }
+
+    pub fn new() -> Self {
+        UnitSphereFace { detail: 6 }
+    }
+}
